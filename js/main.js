e2dd8aff610e8daff15d6575e1c0c3b4b645a94c
LiL <90929523+milkfirst@users.noreply.github.com>
2023-10-18 23:10:19 -0400

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/**
+ * I will need to develop a working contact form
+ *
+ * I want to create simple scrolling animations where text will appear to slide in when you scroll down
+ *
+ * Menu Icon Animations
+ * when you hover over the top menu, it highlights the options that you are hovering on and whichever page you are on, that menu option is highlighted
+ *
+ * Work Experience Examples
+ * When you hover over the icons, the images will slightly zoom in / maybe a summary text will also appear describing the project
+ *
+ * Gallery Function
+ * with each case study, i want a gallery of images to aid the visual process of my descriptions
+ */
